37a0e3916b181e5a9c62f3248188d525ab22ef0d
Amit0129 <amitkumarnayak40@gmail.com>
2022-11-12 15:03:20 +0530

    UsingGenericToshowMultipledataType

--- Generic/Generic/Program.cs
+++ Generic/Generic/Program.cs
@@ -2,44 +2,24 @@
 {
     internal class Program
     {
+        public static void ToPrint<T>(T [] arr)
+        {
+            foreach (T element in arr)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("============================");
+
+        }
         static void Main(string[] args)
         {
             int[] intArray = { 1, 2, 3, 4 };
             double[] dobArray = { 1.1, 2.2, 3.3, 4.4 };
             string[] strArray = { "am", "it", "ku", "ma" };
-            Program.ToPrint(intArray);
-            Program.ToPrint(dobArray);
-            Program.ToPrint(strArray);
+            Program.ToPrint<int>(intArray);
+            Program.ToPrint<double>(dobArray);
+            Program.ToPrint<string>(strArray);
+        }
 
-            
-
- 
-        }
-        public static void ToPrint(int[] arr)
-        {
-            foreach (int element in arr)
-            {
-                Console.WriteLine(element);
-            }
-            Console.WriteLine("============================");
-            
-        }
-        public static void ToPrint(double[] arr)
-        {
-            foreach (double element in arr)
-            {
-                Console.WriteLine(element);
-            }
-            Console.WriteLine("=============================");
-
-        }
-        public static void ToPrint(string[] arr)
-        {
-            foreach (string element in arr)
-            {
-                Console.WriteLine(element);
-            }
-            Console.WriteLine("==============================");
-        }
     }
 }
