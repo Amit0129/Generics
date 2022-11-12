c62019127de34b22274ef1a3a6049403f0cf3ff8
Amit0129 <amitkumarnayak40@gmail.com>
2022-11-12 14:51:29 +0530

    To Show The Element Of an Array

--- Generic/Generic/Program.cs
+++ Generic/Generic/Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] intArray = { 1, 2, 3, 4 };
+            double[] dobArray = { 1.1, 2.2, 3.3, 4.4 };
+            string[] strArray = { "am", "it", "ku", "ma" };
+            Program.ToPrint(intArray);
+            Program.ToPrint(dobArray);
+            Program.ToPrint(strArray);
+
+            
+
+ 
+        }
+        public static void ToPrint(int[] arr)
+        {
+            foreach (int element in arr)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("============================");
+            
+        }
+        public static void ToPrint(double[] arr)
+        {
+            foreach (double element in arr)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("=============================");
+
+        }
+        public static void ToPrint(string[] arr)
+        {
+            foreach (string element in arr)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("==============================");
         }
     }
 }
